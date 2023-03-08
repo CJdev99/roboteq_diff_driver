d5b9e33e596d296250ee7331b624c4954287c806
Chase Devitt <cjdevitt@gmail.com>
2023-03-07 23:46:43 -0600

    update params and fix launch

--- roboteq_diff_driver/launch/roboteq_diff_driver.launch.py
+++ roboteq_diff_driver/launch/roboteq_diff_driver.launch.py
@@ -1,56 +1,49 @@
-import launch
-from launch.actions import DeclareLaunchArgument, ExecuteProcess, SetEnvironmentVariable
-from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+# MIT License
+#
+# Copyright (c) []
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
+
 
 def generate_launch_description():
-    parameters_file_path = PathJoinSubstitution([FindPackageShare('my_package'), 'config', 'my_params.yaml'])
+    # ROS packages
+    pkg_roboteq = get_package_share_directory('roboteq_diff_driver')
 
-    # Declare launch arguments
-    pub_odom_tf = DeclareLaunchArgument('pub_odom_tf', default_value='true', description='Whether to publish the odometry transform')
-    odom_frame = DeclareLaunchArgument('odom_frame', default_value='odom', description='The name of the odom frame')
-    base_frame = DeclareLaunchArgument('base_frame', default_value='base_link', description='The name of the base frame')
-    cmdvel_topic = DeclareLaunchArgument('cmdvel_topic', default_value='cmd_vel', description='The name of the cmdvel topic')
-    odom_topic = DeclareLaunchArgument('odom_topic', default_value='odom', description='The name of the odom topic')
-    port = DeclareLaunchArgument('port', default_value='/dev/ttyACM0', description='The serial port to use')
-    baud = DeclareLaunchArgument('baud', default_value='115200', description='The baud rate to use')
-    open_loop = DeclareLaunchArgument('open_loop', default_value='false', description='Whether to use open loop mode')
-    wheel_circumference = DeclareLaunchArgument('wheel_circumference', default_value='0.3192', description='The wheel circumference in meters')
-    track_width = DeclareLaunchArgument('track_width', default_value='0.4318', description='The track width in meters')
-    encoder_ppr = DeclareLaunchArgument('encoder_ppr', default_value='900', description='The encoder pulses per revolution')
-    encoder_cpr = DeclareLaunchArgument('encoder_cpr', default_value='3600', description='The encoder counts per revolution')
-    max_amps = DeclareLaunchArgument('max_amps', default_value='5.0', description='The maximum motor current in amps')
-    max_rpm = DeclareLaunchArgument('max_rpm', default_value='100', description='The maximum motor RPM')
+    # Config
+    roboteq_config = os.path.join(pkg_roboteq, 'config',
+        'roboteq.yaml')
 
-    # Set environment variables
-    ld_library_path = SetEnvironmentVariable('LD_LIBRARY_PATH', ':/opt/ros/melodic/lib')
-
-    # Define nodes
-    my_node = Node(
-        package='my_package',
-        executable='my_node',
-        parameters=[parameters_file_path],
-        remappings=[('cmd_vel', cmdvel_topic), ('odom', odom_topic)],
-        output='screen'
+    # Nodes
+    roboteq_diff_driver = Node(
+        package='roboteq_diff_driver',
+        executable='driver_dev',
+        name='roboteq_diff_driver',
+        output='screen',
+        parameters=[roboteq_config],
     )
 
-    # Specify the order of execution
-    return launch.LaunchDescription([
-        pub_odom_tf,
-        odom_frame,
-        base_frame,
-        cmdvel_topic,
-        odom_topic,
-        port,
-        baud,
-        open_loop,
-        wheel_circumference,
-        track_width,
-        encoder_ppr,
-        encoder_cpr,
-        max_amps,
-        max_rpm,
-        ld_library_path,
-        my_node,
+    return LaunchDescription([
+        # Nodes
+        roboteq_diff_driver,
     ])
