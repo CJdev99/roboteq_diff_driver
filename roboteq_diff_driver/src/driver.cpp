82b9c775595edbf6e6cbe3dcd112dd4e7f320389
Chase Devitt <55821619+CJdev99@users.noreply.github.com>
2023-03-28 22:26:58 -0500

    Update driver.cpp

--- roboteq_diff_driver/src/driver.cpp
+++ roboteq_diff_driver/src/driver.cpp
@@ -276,11 +276,6 @@ ROS_DEBUG_STREAM("cmdvel speed right: " << right_speed << " left: " << left_spee
     // set minimum to overcome friction if cmd_vel too low
     int32_t right_power = right_speed / wheel_circumference * 60.0 / max_rpm * 1000.0;
     int32_t left_power = left_speed / wheel_circumference * 60.0 / max_rpm * 1000.0;
-    if (right_power < 10 && left_power > 0){
-      right_power = 10
-    }
-    if (left_power < 10 && left_power > 0){
-      left_power = 10
     }
 #ifdef _CMDVEL_DEBUG
 ROS_DEBUG_STREAM("cmdvel power right: " << right_power << " left: " << left_power);
